11893ad2cf52cc29178db0d52ea8cf7426be4a36
moha5145 <mms.jimma@gmail.com>
2022-05-14 02:57:28 +0200

    add mongoose connect

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const formidable = require("express-formidable");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/marvel");
+// mongoose.connect("mongodb://localhost:27017/marvel");
+mongoose.connect(process.env.MONGOOSE_CONNECT);
 
 const app = express();
 
